95c13600a75d82af3ec8985351aa4fd81ff30ebc
atf-sangeeth-subramoniam <s.subramoniam@aitf.co.jp>
2025-07-16 14:41:09 +0900

    Added S3 bucket image storage 2

--- app.py
+++ app.py
@@ -4,12 +4,11 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# configure your bucket name
+# Config
 S3_BUCKET = 'my-flask-images'
 IMAGES_FILE = 'images.txt'
 
-# create S3 client
-
+# Create S3 client using env vars
 session = boto3.Session(
     aws_access_key_id=os.environ.get("AWS_ACCESS_KEY_ID"),
     aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY"),
@@ -19,17 +18,16 @@ session = boto3.Session(
 
 s3_client = session.client("s3")
 
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         if 'file' not in request.files:
-            return 'No file part'
+            return 'No file part in request.'
 
         file = request.files['file']
 
         if file.filename == '':
-            return 'No selected file'
+            return 'No selected file.'
 
         if file:
             # Upload file to S3
@@ -40,9 +38,20 @@ def index():
                 ExtraArgs={'ACL': 'public-read'}
             )
 
-            # Generate the S3 URL
+            # Build the public S3 URL
             image_url = f"https://{S3_BUCKET}.s3.amazonaws.com/{file.filename}"
 
-            # Save URL to text file
+            # Save URL to file
             with open(IMAGES_FILE, 'a') as f:
-                f.write(image_url + '\n')_
+                f.write(image_url + '\n')
+
+    # Always read all stored URLs
+    images = []
+    if os.path.exists(IMAGES_FILE):
+        with open(IMAGES_FILE, 'r') as f:
+            images = [line.strip() for line in f if line.strip()]
+
+    return render_template('index.html', images=images)
+
+if __name__ == '__main__':
+    app.run(debug=True)
